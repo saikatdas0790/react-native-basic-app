b956c80647cc376baa2267dab657f7455c91821f
Saikat Das <saikatdas0790@gmail.com>
2019-01-02 10:13:12 +0530

    Check how justify content determines distribution of children along the primary axis

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { AppRegistry, View } from "react-native";
 
-export default class FlexDirectionBasics extends Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{ flex: 1, flexDirection: "row" }}>
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-between"
+        }}
+      >
         <View
           style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
         />
@@ -16,4 +23,4 @@ export default class FlexDirectionBasics extends Component {
   }
 }
 
-AppRegistry.registerComponent("BasicApp", () => FlexDirectionBasics);
+AppRegistry.registerComponent("BasicApp", () => JustifyContentBasics);
