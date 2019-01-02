1f3b25ec53ec7b7c4f6917c8914cf076f4a2778f
Saikat Das <saikatdas0790@gmail.com>
2019-01-02 10:06:14 +0530

    Set flex direction for horizontal or vertical orientation

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { AppRegistry, View } from "react-native";
 
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, backgroundColor: "powderblue" }} />
-        <View style={{ flex: 1, backgroundColor: "skyblue" }} />
-        <View style={{ flex: 1, backgroundColor: "steelblue" }} />
+      // Try setting `flexDirection` to `column`.
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View
+          style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
+        />
+        <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }} />
+        <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }} />
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent("BasicApp", () => FlexDimensionsBasics);
+AppRegistry.registerComponent("BasicApp", () => FlexDirectionBasics);
