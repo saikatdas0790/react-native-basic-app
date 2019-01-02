58bc917837cbb5921714969731cda4f2c6118f43
Saikat Das <saikatdas0790@gmail.com>
2019-01-02 14:28:20 +0530

    Check how alignItems determines the alignment of children along the secondary axis

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
 import { AppRegistry, View } from "react-native";
 
-export default class JustifyContentBasics extends Component {
+export default class AlignItemsBasics extends Component {
   render() {
     return (
-      // Try setting `justifyContent` to `center`.
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
       // Try setting `flexDirection` to `row`.
       <View
         style={{
           flex: 1,
           flexDirection: "column",
-          justifyContent: "space-between"
+          justifyContent: "center",
+          alignItems: "stretch"
         }}
       >
         <View
           style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
         />
-        <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }} />
-        <View style={{ width: 50, height: 50, backgroundColor: "steelblue" }} />
+        <View style={{ height: 50, backgroundColor: "skyblue" }} />
+        <View style={{ height: 100, backgroundColor: "steelblue" }} />
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent("BasicApp", () => JustifyContentBasics);
+AppRegistry.registerComponent("BasicApp", () => AlignItemsBasics);
