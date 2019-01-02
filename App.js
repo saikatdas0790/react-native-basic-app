86861355ca83c86aeb4eb8cf55959ad342512977
Saikat Das <saikatdas0790@gmail.com>
2019-01-02 17:22:18 +0530

    Using a basic Button component to style and display buttons

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
-import { AppRegistry, Text, TextInput, View } from "react-native";
+import { Alert, AppRegistry, Button, StyleSheet, View } from "react-native";
 
-export default class PizzaTranslator extends Component {
-  state = { text: "" };
+export default class ButtonBasics extends Component {
+  onPressButton() {
+    Alert.alert("You tapped the button!");
+  }
 
   render() {
     return (
-      <View style={{ padding: 10 }}>
-        <TextInput
-          style={{ height: 40 }}
-          placeholder="Type here to translate!"
-          onChangeText={text => this.setState({ text })}
-        />
-        <Text style={{ padding: 10, fontSize: 42 }}>
-          {this.state.text
-            .split(" ")
-            .map(word => word && "🍕")
-            .join(" ")}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.onPressButton} title="Press Me" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this.onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button onPress={this.onPressButton} title="This looks great!" />
+          <Button onPress={this.onPressButton} title="OK!" color="#841484" />
+        </View>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent("AwesomeProject", () => PizzaTranslator);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
+});
+
+AppRegistry.registerComponent("BasicApp", () => ButtonBasics);
