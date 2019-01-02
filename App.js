751bd40a45f111d11d9cbcd9fa3be48e1e3efc84
Saikat Das <saikatdas0790@gmail.com>
2019-01-02 09:13:15 +0530

    Learn how styling works by passing style to prop

--- App.js
+++ App.js
@@ -1,41 +1,28 @@
 import React, { Component } from "react";
-import { AppRegistry, Text, View } from "react-native";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
-class Blink extends Component {
-  state = { isShowingText: true };
-
-  constructor(props) {
-    super(props);
-
-    setInterval(
-      () =>
-        this.setState(previousState => ({
-          isShowingText: !previousState.isShowingText
-        })),
-      1000
-    );
+const styles = StyleSheet.create({
+  bigblue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30
+  },
+  red: {
+    color: "red"
   }
+});
 
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return <Text>{this.props.text}</Text>;
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render() {
     return (
       <View>
-        <Blink text="I love to blink" />
-        <Blink text="Yes blinking is so great" />
-        <Blink text="Why did they ever take this out of HTML" />
-        <Blink text="Look at me look at me look at me" />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
 
-AppRegistry.registerComponent("BasicApp", () => BlinkApp);
+AppRegistry.registerComponent("BasicApp", () => LotsOfStyles);
