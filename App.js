dd222c8a34790a7cfcdbf5012ecad84d76a87faf
Saikat Das <saikatdas0790@gmail.com>
2019-01-03 14:35:50 +0530

    Use Fetch API to fetch JSON data and display  them in a FlatList

--- App.js
+++ App.js
@@ -1,56 +1,44 @@
-import React, { Component } from "react";
-import { AppRegistry, SectionList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { FlatList, ActivityIndicator, Text, View } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: "bold",
-    backgroundColor: "rgba(247,247,247,1)"
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44
+export default class FetchExample extends React.Component {
+  state = { isLoading: true };
+
+  componentDidMount() {
+    return fetch("https://facebook.github.io/react-native/movies.json")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          { isLoading: false, dataSource: responseJson.movies },
+          () => {}
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
-});
 
-export default class SectionListBasics extends Component {
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            { title: "D", data: ["Devin"] },
-            {
-              title: "J",
-              data: [
-                "Jackson",
-                "James",
-                "Jillian",
-                "Jimmy",
-                "Joel",
-                "John",
-                "Julie"
-              ]
-            }
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({ section }) => (
-            <Text style={styles.sectionHeader}>{section.title}</Text>
+      <View style={{ flex: 1, paddingTop: 20 }}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
           )}
-          keyExtractor={(item, index) => index}
+          keyExtractor={({ id }, index) => id}
         />
       </View>
     );
   }
 }
-
-AppRegistry.registerComponent("BasicApp", () => SectionListBasics);
